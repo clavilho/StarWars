e92cb5776f17f963f4b6d32717a5855261a10106
clavilho <jv.clavilho@hotmail.com>
2021-05-05 15:25:36 -0300

    validacao login

--- src/app/views/login/auth.service.ts
+++ src/app/views/login/auth.service.ts
@@ -15,19 +15,27 @@ export class AuthService {
   }
 
   fazerLogin(login: string, senha: string) {
-    console.log('teste')
-    this.loginService.getUser().subscribe(
-      (users) => {
-        this.usuarios = users
-        if (this.usuarios.find(el => el.user === login && el.password === senha)) {
-          this.usuarioAutenticado = true;
-          console.log(this.usuarios)
-          this.router.navigateByUrl('/home');
-        } else {
-          this.usuarioAutenticado = false;
-          alert('Login ou senha invalido');
-        }
-      }
-    )
+    //O CODIGO COMENTADO ABAIXO É A AUTENTICAÇÃO NA FAKE API
+    //PARA QUE ELE SEJA RODADO E ACESSE A API É NECESSARIO RODAR O COMANDO
+    //json-server --watch banco-de-dados.json
+    
+    // console.log('teste')
+    // this.loginService.getUser().subscribe(
+    //   (users) => {
+    //     this.usuarios = users
+    //     if (this.usuarios.find(el => el.user === login && el.password === senha)) {
+    //       this.usuarioAutenticado = true;
+    //       console.log(this.usuarios)
+    //       this.router.navigateByUrl('/home');
+    //     } else {
+    //       this.usuarioAutenticado = false;
+    //       alert('Login ou senha invalido');
+    //     }
+    //   }
+    // )
+
+    if (login === 'admin' && senha === 'admin') {
+      this.router.navigate(['/home'])
+    }
   }
 }
